67369fb383d46cc42a4f44fdf50315aede1c31a8
jessica rocha <jerocha-@student.42sp.org.br>
2026-04-08 17:49:24 -0300

    temp_ex08

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jerocha- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/08 15:34:59 by jerocha-          #+#    #+#             */
+/*   Updated: 2026/04/08 15:35:01 by jerocha-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../aux_funcs.h"
+
+void	ft_sort_int_tab(int *tab, int size);
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+}
+
+int	main(void)
+{
+	int	int_array[10] = {5, 3, 7, 8, 1, 2, 0, 4, 9, 6};
+
+	print_int_array(&int_array[0], 10);
+	return (0);
+}
