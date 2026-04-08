58a19f51cee4834c04f79d4c03184be1f57463e6
jerocha42 <jerocha-@student.42sp.org.br>
2026-04-07 23:51:34 -0300

    ex02

---
+++ ex02/ft_swap.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jerocha- <jerocha-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/07 23:43:36 by jerocha-          #+#    #+#             */
+/*   Updated: 2026/04/07 23:50:01 by jerocha-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_swap(int *a, int *b);
+
+void	ft_swap(int *a, int *b)
+{
+	int	aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
+
+/*
+** int	main(void)
+** {
+** 	int	x;
+** 	int	y;
+** 
+** 	x = 10;
+** 	y = 20;
+** 	printf("Antes da troca: x = %d, y = %d\n", x, y);
+** 	ft_swap(&x, &y);
+** 	printf("Depois da troca: x = %d, y = %d\n", x, y);
+** }
+** 
+*/
