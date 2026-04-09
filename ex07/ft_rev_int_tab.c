a96b38dd5e67550e3fda5e531035d81d960bb59b
jerocha42 <jerocha-@student.42sp.org.br>
2026-04-08 22:34:24 -0300

    ex07 - extract array logic to function

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -3,14 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jerocha- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jerocha- <jerocha-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/08 14:12:43 by jerocha-          #+#    #+#             */
-/*   Updated: 2026/04/08 14:12:52 by jerocha-         ###   ########.fr       */
+/*   Updated: 2026/04/08 20:36:14 by jerocha-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_rev_int_tab(int *tab, int size);
+
+void	invert_position(int *tab, int position_a, int position_b)
+{
+	int	aux;
+
+	aux = tab[position_a];
+	tab[position_a] = tab[position_b];
+	tab[position_b] = aux;
+}
 
 void	ft_rev_int_tab(int *tab, int size)
 {
@@ -22,9 +31,7 @@ void	ft_rev_int_tab(int *tab, int size)
 	end_pos = size - 1;
 	while (start_pos != end_pos && (end_pos - start_pos) > 0)
 	{
-		aux = tab[start_pos];
-		tab[start_pos] = tab[end_pos];
-		tab[end_pos] = aux;
+		invert_position(tab, start_pos, end_pos);
 		start_pos = start_pos + 1;
 		end_pos = end_pos - 1;
 	}
